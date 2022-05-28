3dfd1b068ef764bfa8edc44dac69b75444611c7d
SepehrNamdar <sepehr_gsfc@yahoo.com>
2022-05-28 13:29:25 +0200

    solution

--- src/test/java/PrimeNumbersTest.java
+++ src/test/java/PrimeNumbersTest.java
@@ -6,13 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrimeNumbersTest {
 
-    /**
+    /*
      * Implement PrimeNumbers Class
      */
     @Test
-    public void should_return_first_30_prime_numbers() {
+    public void should_find_prime_numbers_up_to_a_limit() {
         PrimeNumbers primeNumbers = new PrimeNumbers();
+
         List<Integer> actualPrimeNumbers = primeNumbers.calculate(30);
+
         assertThat(actualPrimeNumbers).containsExactly(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
     }
 
